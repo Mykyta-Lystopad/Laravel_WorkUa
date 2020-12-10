29615ec654f731f075921699eb4830212bcfcb23
Nikita-Listopad <niktoring77@gmail.com>
2020-12-10 13:41:58 +0300

    Add table.

---
+++ database/migrations/2020_12_05_153544_create_user_vacancy_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use App\Models\Vacancy;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserVacancyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_vacancy', function (Blueprint $table) {
+//            $table->primary(['user_id', 'vacancy_id']);
+//            $table->foreignIdFor(User::class);
+//            $table->foreignIdFor(Vacancy::class);
+//            $table->timestamps();
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('vacancy_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('vacancy_id')->references('id')->on('vacancies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_vacancy');
+    }
+}
